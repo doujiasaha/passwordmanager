96a14604ec4cc9d2a88368ac8bd32b13bc29be8b
Asakura <Asakura-MBA>
2025-04-19 00:37:54 +0200

    Added search functionality. Swapped to JSON

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random, string
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 def gen():
@@ -15,22 +16,58 @@ def gen():
 
 def save_password():
     
-    website_input = website_box.get()
+    website_input = website_box.get().title()
     user_input = name_box.get()
     password_input = password_box.get()
+    new_data = {
+        website_input: {
+            "email": user_input,
+            "password": password_input,
+        }
+    }
     
     if len(website_input) == 0 or len(password_input) == 0:
         messagebox.showinfo(title="Error", message="You've left some fields empty.")
     else:
-        is_ok = messagebox.askokcancel(title=website_input, message=f"Entry has been saved to file. \nEmail: {user_input} "
-                                                            f"\nPassword: {password_input} \nIs it ok to save?")
+        try:
+            with open("data.json", "r") as file:
+                #Read old data
+                data = json.load(file) 
+        except FileNotFoundError:
+            with open("data.json", "w") as file:
+                json.dump(new_data, file, indent=4)
+        else:
+            #Updatin old data with new data
+            data.update(new_data)
+
+            with open("data.json", "w") as file:
+                #Save updated data
+                json.dump(data, file, indent=4)
+        finally:
+            website_box.delete(0,END)
+            password_box.delete(0,END)
+
+# ---------------------------- SEARCH ENTRY ------------------------------- #
+
+def search_entries():
+    website_input = website_box.get().title()
+    try:
+        with open("data.json", "r") as file:
+            #Read old data
+            data = json.load(file)
+            #Updatin old data with new data
+    except FileNotFoundError:
+        messagebox.showinfo(title="No Entries yet", message="You currently do not have entries in your password manager.")           
+    else:
         
-        if is_ok:
-            with open("data.txt", "a") as file:
-                file.write(f"{website_input} | {user_input} | {password_input} \n")
-                website_box.delete(0,END)
-                #name_box.delete(0,END)
-                password_box.delete(0,END)
+        if website_input in data:
+            user_data = data[website_input]["email"]
+            password_data = data[website_input]["password"]
+        
+            messagebox.showinfo(title=website_input,message=f"Email: {user_data} \n"
+                                f"Password: {password_data}")
+        else:
+            messagebox.showwarning(title=f"No Entry for {website_input}", message="No details for the website exists.") 
     
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -47,8 +84,8 @@ canvas.create_image(100,100,image=lock)
 canvas.grid(column=1, row=0)
 
 #Labels
-website = Label(text="Website:")
-website.grid(column=0,row=1)
+website_label = Label(text="Website:")
+website_label.grid(column=0,row=1)
 
 name = Label(text="Email/Username:")
 name.grid(column=0,row=2)
@@ -57,8 +94,8 @@ password = Label(text="Password:")
 password.grid(column=0,row=3)
 
 #Entries
-website_box = Entry(width=35)
-website_box.grid(column=1,row=1, columnspan=2)
+website_box = Entry(width=18)
+website_box.grid(column=1,row=1)
 website_box.focus()
 
 name_box = Entry(width=35)
@@ -69,7 +106,10 @@ password_box = Entry(width=18)
 password_box.grid(column=1,row=3)
 
 #Buttons
-generate = Button(text="Generate Password", command=gen)
+search = Button(text="Search",command=search_entries ,width=12)
+search.grid(column=2,row=1)
+
+generate = Button(text="Generate Password", command=gen, width=12)
 generate.grid(column=2,row=3)
 
 add_button = Button(text="Add", width=36, command=save_password)
